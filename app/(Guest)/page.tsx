34618cfd361b579bfa5b034ba6620fa332974a71
Maruf <marufmuzahidul@gmail.com>
2025-02-23 22:21:56 +0600

    update

--- app/(Guest)/page.tsx
+++ app/(Guest)/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
-  return <div className="container">Home Page</div>;
+  return <div className="container">Home Page <Button variant={'outline'}>Go Home Page</Button></div>;
 }
