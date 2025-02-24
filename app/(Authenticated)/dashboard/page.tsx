531d30425c63be8554ee15e7611fba772f302bec
skmirajbn <skmirajbn@gmail.com>
2025-02-25 01:54:31 +0600

    updated logedin

--- app/(Authenticated)/dashboard/page.tsx
+++ app/(Authenticated)/dashboard/page.tsx
@@ -3,8 +3,6 @@ import { DateRangePicker } from "@/components/DatePicker/DateRangePicker";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Overview } from "./_partials/OverView";
-import { RecentSales } from "./_partials/RecentSales";
 
 export default function DashboardPage() {
   return (
@@ -14,16 +12,6 @@ export default function DashboardPage() {
         <img src="/examples/dashboard-dark.png" width={1280} height={866} alt="Dashboard" className="hidden dark:block" />
       </div>
       <div className="hidden flex-col md:flex">
-        {/* <div className="border-b">
-          <div className="flex h-16 items-center px-4">
-            <TeamSwitcher />
-            <MainNav className="mx-6" />
-            <div className="ml-auto flex items-center space-x-4">
-              <Search />
-              <UserNav />
-            </div>
-          </div>
-        </div> */}
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
@@ -97,24 +85,6 @@ export default function DashboardPage() {
                     <div className="text-2xl font-bold">+573</div>
                     <p className="text-muted-foreground text-xs">+201 since last hour</p>
                   </CardContent>
-                </Card>
-              </div>
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-                <Card className="col-span-4">
-                  <CardHeader>
-                    <CardTitle>Overview</CardTitle>
-                  </CardHeader>
-                  <CardContent className="pl-2">
-                    <Overview />
-                  </CardContent>
-                </Card>
-                <Card className="col-span-3">
-                  <CardHeader>
-                    <CardTitle>Recent Sales</CardTitle>
-                    <CardContent>
-                      <RecentSales />
-                    </CardContent>
-                  </CardHeader>
                 </Card>
               </div>
             </TabsContent>
