891ac2b8ce3236a91dc7632c5b76711b183f505b
skmirajbn <skmirajbn@gmail.com>
2025-02-25 23:35:37 +0600

    updated sidebar

--- app/(Authenticated)/layout.tsx
+++ app/(Authenticated)/layout.tsx
@@ -1,6 +1,6 @@
 import AuthHeader from "@/components/Authenticated/header/AuthHeader";
 import AppSidebar from "@/components/Guest/AppSidebar/AppSidebar";
-import { SidebarInset } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import AuthenticatedRoute from "@/components/wrappers/AuthenticatedRoute";
 
 type Props = {
@@ -9,13 +9,15 @@ type Props = {
 const layout: React.FC<Props> = ({ children }) => {
   return (
     <div>
-      <AppSidebar />
-      <SidebarInset>
-        <AuthHeader />
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <AuthenticatedRoute>{children}</AuthenticatedRoute>
-        </div>
-      </SidebarInset>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <AuthHeader />
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <AuthenticatedRoute>{children}</AuthenticatedRoute>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
     </div>
   );
 };
