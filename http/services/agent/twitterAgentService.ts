ac838e8f11f9bc0ef0d520a3c37ae0570bed410e
skmirajbn <skmirajbn@gmail.com>
2025-02-27 01:45:31 +0600

    added twitter

--- http/services/agent/twitterAgentService.ts
+++ http/services/agent/twitterAgentService.ts
@@ -1,24 +1,3 @@
-import { Scraper, Tweet } from "agent-twitter-client";
-
-class TwitterAgentService {
-  private readonly scraper: Scraper;
-  private readonly username: string;
-  private readonly password: string;
-
-  constructor({ username, password }: { username: string; password: string }) {
-    this.scraper = new Scraper();
-    this.username = username;
-    this.password = password;
-  }
-
-  async login(): Promise<this> {
-    await this.scraper.login(this.username, this.password);
-    return this;
-  }
-
-  async getLatestTweet(text: string): Promise<void | Tweet | null> {
-    return await this.scraper.getLatestTweet(text);
-  }
-}
+class TwitterAgentService {}
 
 export default TwitterAgentService;
