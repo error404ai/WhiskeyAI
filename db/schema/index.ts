131e6e871da8e5eba5712e7557313514d515dc81
skmirajbn <skmirajbn@gmail.com>
2025-03-04 00:57:54 +0600

    udpated agent trigger

--- db/schema/index.ts
+++ db/schema/index.ts
@@ -1,5 +1,6 @@
 export * from "./agentPlatformsTable";
 export * from "./agentsTable";
 export * from "./agentTriggersTable";
+export * from "./functionsTable";
 export * from "./rolesTable";
 export * from "./usersTable";
---
+++ http/services/agent/AgentTriggerService.ts
@@ -0,0 +1,41 @@
+import { db } from "@/db/db";
+import { agentTriggersTable } from "@/db/schema";
+import { AgentTrigger } from "@/db/schema/agentTriggersTable";
+import { agentTriggerCreateSchema } from "@/http/zodSchema/agentTriggerCreateSchema";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+import AuthService from "../authService";
+import { AgentService } from "./agentService";
+
+export class AgentTriggerService {
+  static async createAgentTrigger(data: z.infer<typeof agentTriggerCreateSchema>) {
+    const agent = await AgentService.getAgentByUuid(data.agentUuid);
+    if (!agent) throw new Error("Agent not found");
+    const authUser = await AuthService.getAuthUser();
+    if (!authUser) throw new Error("User not authenticated");
+    if (Number(authUser.id) !== agent.userId) throw new Error("User not authenticated");
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { agentUuid, ...triggerData } = data;
+    const res = await db.insert(agentTriggersTable).values({
+      ...triggerData,
+      agentId: agent.id,
+      interval: Number(triggerData.interval),
+    });
+    if (res) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  static async getAgentTriggers(agentUuid: string): Promise<AgentTrigger[]> {
+    const authUser = await AuthService.getAuthUser();
+    const agent = await AgentService.getAgentByUuid(agentUuid);
+    if (!agent || !authUser) throw new Error("User not authenticated");
+
+    if (Number(authUser.id) !== agent.userId) throw new Error("User not authenticated");
+
+    return await db.query.agentTriggersTable.findMany({
+      where: eq(agentTriggersTable.agentId, agent.id),
+    });
+  }
+}
