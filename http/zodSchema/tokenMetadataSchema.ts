734d7fd447ba330afc776fa1ef118ec5de67e190
skmirajbn <skmirajbn@gmail.com>
2025-03-10 02:07:13 +0600

    updated schema

--- http/zodSchema/tokenMetadataSchema.ts
+++ http/zodSchema/tokenMetadataSchema.ts
@@ -4,7 +4,7 @@ export const tokenMetadataSchema = z.object({
   launchType: z.enum(["no_token", "new_token", "existing_token"]), //state
   contractAddress: z.string().optional(),
   buyAmount: z.string().nonempty({ message: "Buy amount is required" }),
-  file: z.any().optional(),
+  file: z.any(),
   name: z.string().min(1, "Name is required"),
   symbol: z.string().min(1, "Symbol is required"),
   description: z.string().min(1, "Description is required"),
