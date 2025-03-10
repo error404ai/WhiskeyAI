7be93cb637dfd00383bda46766a6799f1168c588
skmirajbn <skmirajbn@gmail.com>
2025-03-10 20:54:00 +0600

    updated seeder

--- http/services/userService.ts
+++ http/services/userService.ts
@@ -99,7 +99,7 @@ class UserService {
     }
   }
 
-  private static generateCustomerId() {
+  static generateCustomerId() {
     const now = new Date();
     const year = now.getFullYear().toString().slice(-2);
     const month = String(now.getMonth() + 1).padStart(2, "0");
