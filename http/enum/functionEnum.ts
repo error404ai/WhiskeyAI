0fe7d3eaac985415c1a8384df635303bde8b32dd
skmirajbn <skmirajbn@gmail.com>
2025-03-23 17:10:40 +0600

    integrated descreener methods

--- http/enum/functionEnum.ts
+++ http/enum/functionEnum.ts
@@ -10,4 +10,10 @@ export enum functionEnum {
   RPC_getBlock = "RPC_getBlock",
   DEX_getLatestTokenProfiles = "DEX_getLatestTokenProfiles",
   DEX_getLatestBoostedTokens = "DEX_getLatestBoostedTokens",
+  DEX_getTopBoostedTokens = "DEX_getTopBoostedTokens",
+  DEX_getTokenOrders = "DEX_getTokenOrders",
+  DEX_getPairsByChainAndPairAddress = "DEX_getPairsByChainAndPairAddress",
+  DEX_searchPairs = "DEX_searchPairs",
+  DEX_getTokenPairs = "DEX_getTokenPairs",
+  DEX_getTokensByAddress = "DEX_getTokensByAddress",
 }
