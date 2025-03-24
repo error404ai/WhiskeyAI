cd13735ff9021891a9ba2dad23a048f3380e1ce1
skmirajbn <skmirajbn@gmail.com>
2025-03-24 22:30:43 +0600

    updated function

--- http/services/Rpc/DexscreenerService.ts
+++ http/services/Rpc/DexscreenerService.ts
@@ -4,8 +4,20 @@ export class DexscreenerService {
     const response = await fetch(this.dexscreenerEndpoint + "/token-profiles/latest/v1", {
       method: "GET",
     });
+
     const data = await response.json();
-    return data;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const mostRecentItem = data.reduce((latest: any, current: any) => {
+      const latestTimestampMatch = latest.openGraph.match(/timestamp=(\d+)/);
+      const currentTimestampMatch = current.openGraph.match(/timestamp=(\d+)/);
+
+      const latestTimestamp = latestTimestampMatch ? parseInt(latestTimestampMatch[1], 10) : 0;
+      const currentTimestamp = currentTimestampMatch ? parseInt(currentTimestampMatch[1], 10) : 0;
+
+      return currentTimestamp > latestTimestamp ? current : latest;
+    }, data[0]);
+
+    return [mostRecentItem];
   }
   async getLatestBoostedTokens() {
     const response = await fetch(this.dexscreenerEndpoint + "/token-boosts/latest/v1", {
@@ -14,7 +26,7 @@ export class DexscreenerService {
     const data = await response.json();
     return data;
   }
-  
+
   async getTopBoostedTokens() {
     const response = await fetch(this.dexscreenerEndpoint + "/token-boosts/top/v1", {
       method: "GET",
@@ -22,7 +34,7 @@ export class DexscreenerService {
     const data = await response.json();
     return data;
   }
-  
+
   async getTokenOrders(chainId: string, tokenAddress: string) {
     const response = await fetch(this.dexscreenerEndpoint + `/orders/v1/${chainId}/${tokenAddress}`, {
       method: "GET",
@@ -30,7 +42,7 @@ export class DexscreenerService {
     const data = await response.json();
     return data;
   }
-  
+
   async getPairsByChainAndPairAddress(chainId: string, pairId: string) {
     const response = await fetch(this.dexscreenerEndpoint + `/latest/dex/pairs/${chainId}/${pairId}`, {
       method: "GET",
@@ -38,7 +50,7 @@ export class DexscreenerService {
     const data = await response.json();
     return data;
   }
-  
+
   async searchPairs(query: string) {
     const response = await fetch(this.dexscreenerEndpoint + `/latest/dex/search?q=${encodeURIComponent(query)}`, {
       method: "GET",
@@ -46,7 +58,7 @@ export class DexscreenerService {
     const data = await response.json();
     return data;
   }
-  
+
   async getTokenPairs(chainId: string, tokenAddress: string) {
     const response = await fetch(this.dexscreenerEndpoint + `/token-pairs/v1/${chainId}/${tokenAddress}`, {
       method: "GET",
@@ -54,7 +66,7 @@ export class DexscreenerService {
     const data = await response.json();
     return data;
   }
-  
+
   async getTokensByAddress(chainId: string, tokenAddresses: string) {
     const response = await fetch(this.dexscreenerEndpoint + `/tokens/v1/${chainId}/${tokenAddresses}`, {
       method: "GET",
