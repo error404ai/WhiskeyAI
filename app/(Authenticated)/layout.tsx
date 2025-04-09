d7037334b2875eb0db944347c24af11ac84dd080
skmirajbn <skmirajbn@gmail.com>
2025-04-10 02:17:33 +0600

    Add page loading indicator

--- app/(Authenticated)/layout.tsx
+++ app/(Authenticated)/layout.tsx
@@ -1,22 +1,28 @@
+"use client";
 import AuthHeader from "@/components/Authenticated/header/AuthHeader";
 import AppSidebar from "@/components/Guest/AppSidebar/AppSidebar";
+import PageLoading from "@/components/Loading/PageLoading";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { useOnNavigate } from "@/hooks/useOnNavigate";
 
 type Props = {
   children: React.ReactNode;
 };
-const layout: React.FC<Props> = ({ children }) => {
+const Layout: React.FC<Props> = ({ children }) => {
+  const loading = useOnNavigate();
   return (
     <div>
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
           <AuthHeader />
-          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            {!loading && children} {loading && <PageLoading />}
+          </div>
         </SidebarInset>
       </SidebarProvider>
     </div>
   );
 };
 
-export default layout;
+export default Layout;
--- app/(Authenticated)/loading.tsx
+++ app/(Authenticated)/loading.tsx
@@ -1,39 +1,5 @@
-import { Skeleton } from "@/components/ui/skeleton";
+import PageLoading from "@/components/Loading/PageLoading";
 
 export default function Loading() {
-  return (
-    <div className="mx-auto w-full max-w-6xl p-4 md:p-6">
-      {/* Header skeleton */}
-      <div className="mb-8 space-y-2">
-        <Skeleton className="h-8 w-2/3 md:w-1/3" />
-        <Skeleton className="h-4 w-full md:w-2/3" />
-      </div>
-
-      {/* Content skeleton */}
-      <div className="space-y-6">
-        {/* Card grid */}
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {[...Array(3)].map((_, i) => (
-            <div key={i} className="space-y-3 rounded-lg border p-4">
-              <Skeleton className="h-40 w-full rounded-md" />
-              <Skeleton className="h-5 w-3/4" />
-              <Skeleton className="h-4 w-full" />
-              <Skeleton className="h-4 w-2/3" />
-              <div className="flex justify-between pt-2">
-                <Skeleton className="h-8 w-20" />
-                <Skeleton className="h-8 w-8 rounded-full" />
-              </div>
-            </div>
-          ))}
-        </div>
-
-        {/* Pagination skeleton */}
-        <div className="flex items-center justify-center gap-2 pt-4">
-          <Skeleton className="h-8 w-8 rounded-md" />
-          <Skeleton className="h-8 w-8 rounded-md" />
-          <Skeleton className="h-8 w-8 rounded-md" />
-        </div>
-      </div>
-    </div>
-  );
+  return <PageLoading />;
 }
