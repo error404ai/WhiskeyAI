cfc310c78249f01b5fb59c74def4df08796ad711
skmirajbn <skmirajbn@gmail.com>
2025-04-12 18:09:32 +0600

    Improve authenticated layout with client side wrapper.

--- app/(Authenticated)/layout.tsx
+++ app/(Authenticated)/layout.tsx
@@ -1,38 +1,25 @@
-"use client";
+import ClientLayout from "@/components/Authenticated/ClientLayout";
 import AuthHeader from "@/components/Authenticated/header/AuthHeader";
-import { AnimatePresence, motion } from "@/components/ClientWrappers/motion";
 import AppSidebar from "@/components/Guest/AppSidebar/AppSidebar";
-import PageLoading from "@/components/Loading/PageLoading";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { useRouteLoading } from "@/hooks/useRouteLoading";
+import AuthenticatedRoute from "@/components/wrappers/AuthenticatedRoute";
 
 type Props = {
   children: React.ReactNode;
 };
 
+// Server component
 const Layout: React.FC<Props> = ({ children }) => {
-  const { isLoading } = useRouteLoading();
-
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <AuthHeader />
-        <div className="relative flex flex-1 flex-col gap-4 p-4 pt-0">
-          <AnimatePresence mode="wait">
-            {isLoading ? (
-              <motion.div key="loading" initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -10 }} transition={{ duration: 0.3, ease: "easeInOut" }} className="w-full">
-                <PageLoading />
-              </motion.div>
-            ) : (
-              <motion.div key="content" initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -10 }} transition={{ duration: 0.3, ease: "easeInOut" }} className="w-full">
-                {children}
-              </motion.div>
-            )}
-          </AnimatePresence>
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+    <AuthenticatedRoute>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <AuthHeader />
+          <ClientLayout>{children}</ClientLayout>
+        </SidebarInset>
+      </SidebarProvider>
+    </AuthenticatedRoute>
   );
 };
 
