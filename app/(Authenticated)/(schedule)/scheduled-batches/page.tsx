1dabf363d5ece5aa261614d9d7beb18fba3dda15
skmirajbn <skmirajbn@gmail.com>
2025-04-14 18:13:57 +0600

    Move scheduled posts table to separate page.

--- app/(Authenticated)/(schedule)/scheduled-batches/page.tsx
+++ app/(Authenticated)/(schedule)/scheduled-batches/page.tsx
@@ -1,5 +1,27 @@
-const page = () => {
-  return <div>page</div>;
+"use client";
+
+import { Button } from "@/components/ui/button";
+import ScheduledTweetsTable from "../schedule-tweet/_partials/ScheduledTweetsTable";
+import { useRouter } from "next/navigation";
+
+const ScheduledBatchesPage = () => {
+  const router = useRouter();
+  
+  return (
+    <div className="container p-4">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-bold">Scheduled Posts</h1>
+        <Button 
+          onClick={() => router.push("/schedule-tweet")}
+          className="bg-blue-600 hover:bg-blue-700"
+        >
+          Create New Schedule
+        </Button>
+      </div>
+      
+      <ScheduledTweetsTable />
+    </div>
+  );
 };
 
-export default page;
+export default ScheduledBatchesPage;
