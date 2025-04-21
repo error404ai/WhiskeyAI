ac99aa4879a6178419269f59e8dda757a566b014
skmirajbn <skmirajbn@gmail.com>
2025-04-21 23:58:31 +0600

    Updated schema path for Drizzle config.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./server/drizzle",
-  schema: "./db/schema",
+  schema: "./server/db/schema",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
