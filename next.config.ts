bb2391a0869265076ce2084b7ed2c3699b1689d4
arav deshmuh <pseudonym@example.com>
2025-12-09 00:46:52 +0600

    Organize config files and update dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  serverActions: {
-    bodySizeLimit: "20mb",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "20mb",
+    },
   },
 };
 
